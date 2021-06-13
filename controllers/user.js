727c0f444817878ed626f1c26d8d4cad902ddeee
Chris Delarue <tophy.cd1@gmail.com>
2021-06-13 23:31:58 +0200

    update controller user

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const MaskData = require('maskdata');
-
+const db = require('../db_connect');
 require('dotenv').config();
 
 const User = require('../models/user');
@@ -26,6 +26,7 @@ exports.signup = (req, res, next) =>{
 
             //on passe email trouvé dans le corps de la requête
             email:maskedEmail,
+<<<<<<< Updated upstream
             //récupération du mdp hashé de bcrypt
             password: hash
            
@@ -37,12 +38,40 @@ exports.signup = (req, res, next) =>{
             console.log(error)
             res.status(500).json({error})
         } );
+=======
+            firstname: req.body.firstname,
+            surname: req.body.surname,
+            password: hash,
+            isAdmin : req.body.isAdmin
+        });
+            db.query(`INSERT INTO users SET ?`, user,  function(error, results, fields){
+                if(error){
+                    res.status(400).json({error :'error'});
+                }
+            db.query(`SELECT * FROM users WHERE email = ?`, [maskedEmail], function(error, results, fields) {
+                if(error){
+                    res.status(404).json({error});
+                }
+                    res.status(201).json({
+                        userId : results[0].id,
+                        isAdmin : results[0].isAdmin,
+                        token: jwt.sign(
+                            {userId: results[0].id,
+                            isAdmin : results[0].isAdmin},
+                            process.env.TOKEN_SECRET,
+                            {
+                            expiresIn: '24h'
+                        })
+                    });
+            });
+        });
+>>>>>>> Stashed changes
     })
     .catch(error => res.status(500).json({error}));
 };
 
 exports.login = (req, res, next) => {
-
+ 
     const emailMask2Options = {
         maskWith: "*", 
         unmaskedStartCharactersBeforeAt: 0,
@@ -52,33 +81,28 @@ exports.login = (req, res, next) => {
     
     const maskedEmail = MaskData.maskEmail2(req.body.email,emailMask2Options);
 
-    User.findOne( {
-        email : maskedEmail
-      
-    })
-    .then(user => {
-        if(!user){
-           
-            return res.status(401).json(
-                {error: 'Utilisateur non trouvé !'}
-          );
+   db.query(`SELECT * FROM users WHERE email= ? `, [maskedEmail], function(error, results, fields) {
+        if (results.length === 0) {
+             return res.status(401).json({error: 'Utilisateur non trouvé !'});
         }
-        bcrypt.compare(req.body.password, user.password)
+  
+        bcrypt.compare(req.body.password, results[0].password)
         .then(valid => {
             if(!valid){
                 return res.status(404).json({error: 'Veuillez vérifier votre émail et/ou votre mot de passe !'});
             }
            res.status(200).json({
-                userId : user._id,
-                token: jwt.sign(
-                    {userId: user._id},
-                    process.env.TOKEN_SECRET,
-                    {
-                    expiresIn: '900s'
+            userId : results[0].id,
+            isAdmin : results[0].isAdmin,
+            token: jwt.sign(
+                {userId: results[0].id,
+                isAdmin : results[0].isAdmin},
+                process.env.TOKEN_SECRET,
+                {
+                expiresIn: '900s'
                 })
             });
         })
         .catch(error => res.status(500).json({error}));
-    })
-    .catch(error=> res.status(500).json({error}));
+    });
 };
