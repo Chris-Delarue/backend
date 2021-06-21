f96f8c3bfec199446b8ce5bb4fb53dc2fa512aad
Chris Delarue <tophy.cd1@gmail.com>
2021-06-21 12:33:18 +0200

    Mise en place groupomania.sql

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ const userCtrl = require('../controllers/post');
 
 router.get('/',auth,   userCtrl.getAllPost);
 router.post('/', auth, userCtrl.newPost);
-router.get('/:userId', auth,userCtrl.getOnePost);
+router.get('/:userId', auth, userCtrl.getOnePost);
 router.delete('/:userId', auth,  userCtrl.deleteOnePost);
 //router.put('/:userId',  auth,userCtrl.modifyOnePost);
 //router.get('/user:userId/post', auth, userCtrl.getUserPost);
