9b39fe0a792e2ba7abab9269a1cbb60c1a0e3057
Chris Delarue <tophy.cd1@gmail.com>
2021-07-24 17:32:37 +0200

    update controllers user cryptoJS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const rateLimit = require("express-rate-limit");
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -22,12 +22,12 @@ app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => { 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
- res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+//app.use((req, res, next) => { 
+  //res.setHeader('Access-Control-Allow-Origin', '*');
+  //res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+ //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  //next();
+//});
 
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,70 +1,59 @@
 const bcrypt = require('bcrypt');
-const MaskData = require('maskdata');
+const CryptoJS = require("crypto-js");
 const UserRepository = require('../repository/user');
-
+require('dotenv').config();
 
 let userRepository = new UserRepository();
 
 exports.signup = (req, res, next) =>{
      
-        const emailMask2Options = {
-            maskWith: "*", 
-            unmaskedStartCharactersBeforeAt: 0,
-            unmaskedEndCharactersAfterAt: 0,
-            maskAtTheRate: false
-        };
-        const maskedEmail = MaskData.maskEmail2(req.body.email,emailMask2Options);
-
-        let email = maskedEmail;
+        let emailCrypted = CryptoJS.AES.encrypt(req.body.email, process.env.SECRET_PHRASE).toString();
+        let emailHash = CryptoJS.MD5(req.body.email).toString();
         let firstname = req.body.firstname;
         let surname = req.body.surname;
         let password = req.body.password;
         let passwordConfirm = req.body.passwordConfirm;
        
-        if(password === passwordConfirm) {
-        bcrypt.hash(password, 10)
+            if(password === passwordConfirm)
+             {
+            bcrypt.hash(password, 10)
             .then(hash => {
-                let mysqlInsert = [email, firstname,  surname, hash, hash];
-                
+                let mysqlInsert = [emailCrypted, emailHash, firstname,  surname, hash, hash];
+               
                 userRepository.signup(mysqlInsert)
-                
+               
                 .then((response) => {
                     res.status(201).json(response);
                     
                 })
                 .catch((error) => {
                     res.status(400).json(error);
+                
                 });
-                
-                
             })
             .catch((error) => res.status(500).json(error));
-        } 
+        }
+        
+        
 };
 
 exports.login =  (req, res, next) => {
  
-    const emailMask2Options = {
-        maskWith: "*", 
-        unmaskedStartCharactersBeforeAt: 0,
-        unmaskedEndCharactersAfterAt: 0,
-        maskAtTheRate: false
-    }
+    let emailHash = CryptoJS.MD5(req.body.email).toString();
     
-    const maskedEmail = MaskData.maskEmail2(req.body.email,emailMask2Options);
-
-   let email = maskedEmail;
+   let email = emailHash ;
    let password = req.body.password;
    let mysqlInsert = [email];
+
    userRepository.login(mysqlInsert, password)
    
         .then((response) => {
             res.status(200).json(response);
+           console.log(response);
          })
         .catch((error) => {
             res.status(400).json(error);
         });
-
 };
 
 exports.deleteAccount = (req, res, next) => {
@@ -72,12 +61,13 @@ exports.deleteAccount = (req, res, next) => {
     let mysqlInsert = req.params.userId;
 
     userRepository.deleteAccount(mysqlInsert)
-   
+    
     .then((response) => {
         res.status(200).json(response);
+        console.log(response)
     })
     .catch((error) => {
-        console.log(error);
         res.status(400).json(error);
+        console.log(error);
     });
 };
